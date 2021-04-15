88da9b6402105c0bc02e0b8d644cafa7849f9551
OerhortStorMan <l.winelid@gmail.com>
2021-04-15 23:26:27 +0200

    mmm game

--- Racing Game/GameObject.cs
+++ Racing Game/GameObject.cs
@@ -10,6 +10,13 @@ namespace Racing_Game
             Raylib.DrawRectangle((int)playerOne.PosX, (int)playerOne.PosY, playerOne.width, playerOne.length, playerOne.carColor);
 
             Raylib.DrawRectangle((int)playerTwo.PosX, (int)playerTwo.PosY, playerTwo.width, playerTwo.length, playerTwo.carColor);
+        
+            Raylib.DrawText("P1: " + playerOne.lapScore.ToString(), (Window.windowW/2)-(Window.windowH/4)-150, 5, 100, Color.WHITE);
+            
+            Raylib.DrawText("P2: " + playerTwo.lapScore.ToString(), (Window.windowW/2)+(Window.windowH/4)-150, 5, 100, Color.WHITE);
+
+
+
         }
 
         
